dc80c229cdbd0f4ba1f84786b06d1ca6e198a625
Mayuresh94 <mayureshkutte@gmail.com>
2020-01-12 00:03:39 +0530

    changes as requested

--- src/main/java/com/kodekonveyor/market/MarketConstants.java
+++ src/main/java/com/kodekonveyor/market/MarketConstants.java
@@ -8,4 +8,11 @@ public class MarketConstants {
       "Please enter valid payment details";
 
   public static final String KODEKONVEYOR_SALES_ROLE = "kodekonveyor_sales";
+
+  public static final String PAYPAL = "paypal";
+
+  public static final String SEPA = "sepa";
+
+  public static final String TRANSFERWISE = "transferwise";
+
 }
