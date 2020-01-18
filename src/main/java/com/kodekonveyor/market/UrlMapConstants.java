016c3c088b087b9224df729bacd0405c7b72a93b
mag <m4gw4s@gmail.com>
2020-01-18 09:49:03 +0100

    fix pmd violations in RegistrationController, use tagged metamodel version

--- src/main/java/com/kodekonveyor/market/UrlMapConstants.java
+++ src/main/java/com/kodekonveyor/market/UrlMapConstants.java
@@ -6,6 +6,7 @@ public class UrlMapConstants {
   public static final String LIST_LEAD_PATH = "/member/lead";
   public static final String LOGIN_PATH = "/member/login";
   public static final String PAYMENT_UPDATE_PATH = "/update_payment";
+  public static final String REGISTER_USER_PATH = "register";
   public static final String SHOW_USER_PATH = "/member/user";
 
 }
