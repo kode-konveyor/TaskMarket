57743fed7f6e2322cb56c3b1d713429be3520cf1
Meenal <mbsirsikar@gmail.com>
2020-06-02 15:53:34 +0530

    build issue fix

--- src/main/java/com/kodekonveyor/market/tasks/UpdateGithubIssueService.java
+++ src/main/java/com/kodekonveyor/market/tasks/UpdateGithubIssueService.java
@@ -1,5 +1,8 @@
 package com.kodekonveyor.market.tasks;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UpdateGithubIssueService {
 
   public void call(final TaskEntity taskEntity) {
