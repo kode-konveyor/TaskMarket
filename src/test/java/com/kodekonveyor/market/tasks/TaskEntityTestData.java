002033482b114260e50e27b6ab7035d4ba9d01ca
Mayuresh94 <mayureshkutte@gmail.com>
2020-06-30 19:57:55 +0530

    Fixed PMD TaskEntityTestData - has a total cyclomatic complexity of 21

--- src/test/java/com/kodekonveyor/market/tasks/TaskEntityTestData.java
+++ src/test/java/com/kodekonveyor/market/tasks/TaskEntityTestData.java
@@ -32,37 +32,8 @@ public class TaskEntityTestData {
     return taskEntity;
   }
 
-  public static TaskEntity getStatusGrabbedOverThreeDays() {
-    final TaskEntity taskEntity = get();
-    taskEntity.setStatus(TaskStatusEnum.IN_PROGRESS);
-    taskEntity.setGrabDate(TaskTestData.DATE_OLDER_THAN_THREE_DAYS);
-    return taskEntity;
-  }
-
-  public static TaskEntity getStatusUngrabbed() {
-    final TaskEntity taskEntity = get();
-    taskEntity.setGrabDate(null);
-    taskEntity.setMarketUser(null);
-    taskEntity.setStatus(TaskStatusEnum.UP_FOR_GRAB);
-    return taskEntity;
-  }
-
-  public static TaskEntity getStatusGrabbedExactlyThreeDays() {
-    final TaskEntity taskEntity = get();
-    taskEntity.setStatus(TaskStatusEnum.IN_PROGRESS);
-    taskEntity.setGrabDate(TaskTestData.DATE_THREE_DAYS_SINCE_GRABBED);
-    return taskEntity;
-  }
-
-  public static TaskEntity getStatusGrabbedForFourDays() {
-    final TaskEntity taskEntity = get();
-    taskEntity.setStatus(TaskStatusEnum.IN_PROGRESS);
-    taskEntity.setGrabDate(TaskTestData.DATE_FOUR_DAYS_SINCE_GRABBED);
-    return taskEntity;
-  }
-
   public static TaskEntity getPullRequestIssuedTask() {
-    final TaskEntity taskEntity = get();
+    final TaskEntity taskEntity = TaskEntityTestData.get();
     taskEntity.setStatus(TaskStatusEnum.IN_PROGRESS);
     taskEntity.setGrabDate(TaskTestData.DATE_OLDER_THAN_THREE_DAYS);
     return taskEntity;
@@ -144,13 +115,13 @@ public class TaskEntityTestData {
   }
 
   public static TaskEntity getServiceDifferent() {
-    final TaskEntity taskEntity = TaskEntityTestData.get();
+    final TaskEntity taskEntity = get();
     taskEntity.setService(TaskTestData.OTHER_SERVICE);
     return taskEntity;
   }
 
   public static TaskEntity getBehaviourDifferent() {
-    final TaskEntity taskEntity = TaskEntityTestData.get();
+    final TaskEntity taskEntity = get();
     taskEntity.setBehaviour(TaskTestData.OTHER_BEHAVIOUR);
     return taskEntity;
   }
